b280b4af037e48d586ac1938e5d4c08d2d43c2a1
hudson <crocodiledungey@icloud.com>
2024-10-31 22:11:49 +0930

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
-
+// .eslintrc.js
 module.exports = {
   extends: ["next/core-web-vitals", "next/typescript"],
   rules: {
     "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-explicit-any": "off"
   },
 };
