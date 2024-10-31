7fa7777308373519be271e2a2d348312c43f45f4
hudson <crocodiledungey@icloud.com>
2024-10-31 22:17:12 +0930

    rm

--- types/mainTypes.ts
+++ types/mainTypes.ts
@@ -14,7 +14,7 @@ export interface AccountDetails {
     uid: string;
     email: string | null;
     displayName: string | null;
-    memberShipTier: string;
+    memberShipTier?: string;
     photoURL?: string | null;
     country?: string | null; 
     mainActivity?: string | null; 
